298d1ee23c1b914c8a61debdb87e287d46814d59
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-09 21:51:20 +0500

    Create IfExec.java

---
+++ src/main/java/com/ifexec/IfExec.java
@@ -0,0 +1,56 @@
+package com.ifexec;
+
+import com.ifexec.commands.IfCommand;
+import com.ifexec.commands.IfTabCompleter;
+import com.ifexec.listeners.TriggerListener;
+import com.ifexec.manager.ConfigManager;
+import com.ifexec.manager.Messages;
+import com.ifexec.manager.TriggerManager;
+import com.ifexec.manager.UndoManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IfExec extends JavaPlugin {
+
+    private static IfExec instance;
+    private ConfigManager configManager;
+    private Messages messages;
+    private TriggerManager triggerManager;
+    private UndoManager undoManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // ensure data folder and default resources
+        saveDefaultConfig();
+        saveResource("messages.yml", false);
+
+        this.configManager = new ConfigManager(this);
+        this.messages = new Messages(this);
+        this.triggerManager = new TriggerManager(this);
+        this.undoManager = new UndoManager(this, triggerManager);
+
+        // register command and tab completer
+        IfCommand cmd = new IfCommand(this);
+        getCommand("if").setExecutor(cmd);
+        getCommand("if").setTabCompleter(new IfTabCompleter(this));
+
+        // register listener
+        getServer().getPluginManager().registerEvents(new TriggerListener(this), this);
+
+        getLogger().info("IfExec enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        if (triggerManager != null) triggerManager.saveAll();
+        instance = null;
+    }
+
+    public static IfExec getInstance() { return instance; }
+
+    public ConfigManager getConfigManager() { return configManager; }
+    public Messages getMessages() { return messages; }
+    public TriggerManager getTriggerManager() { return triggerManager; }
+    public UndoManager getUndoManager() { return undoManager; }
+}
