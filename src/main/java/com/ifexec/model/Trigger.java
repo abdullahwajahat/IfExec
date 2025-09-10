5633abf6d0b795b33d292557dcb235e739230574
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-10 23:29:46 +0500

    Update Trigger.java

--- src/main/java/com/ifexec/model/Trigger.java
+++ src/main/java/com/ifexec/model/Trigger.java
@@ -46,7 +46,7 @@ public class Trigger {
         return t;
     }
 
-    // getters/setters (concise)
+    // getters/setters
     public String getName() { return name; } public void setName(String name) { this.name = name; }
     public Type getType() { return type; } public void setType(Type type) { this.type = type; }
     public String getWorld() { return world; } public void setWorld(String world) { this.world = world; }
