f3f90fe2dede1aca8ee309df687307ed2380bf7b
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-10 21:32:14 +0500

    Update ConfigManager.java

--- src/main/java/com/ifexec/manager/ConfigManager.java
+++ src/main/java/com/ifexec/manager/ConfigManager.java
@@ -1,36 +1,12 @@
 package com.ifexec.manager;
 
-
 import com.ifexec.IfExec;
-import org.bukkit.configuration.file.YamlConfiguration;
-
-
-import java.io.File;
-import java.io.IOException;
-
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigManager {
-private final IfExec plugin;
-private final File dataFile;
-private YamlConfiguration dataConfig;
+    private final IfExec plugin;
 
+    public ConfigManager(IfExec plugin) { this.plugin = plugin; }
 
-public ConfigManager(IfExec plugin) {
-this.plugin = plugin;
-dataFile = new File(plugin.getDataFolder(), "config.yml");
-if (!dataFile.exists()) plugin.saveResource("config.yml", false);
-dataConfig = YamlConfiguration.loadConfiguration(dataFile);
-}
-
-
-public YamlConfiguration getConfig() { return dataConfig; }
-
-
-public void saveConfig() {
-try {
-dataConfig.save(dataFile);
-} catch (IOException e) {
-plugin.getLogger().severe("Failed to save config.yml: " + e.getMessage());
-}
-}
+    public FileConfiguration getConfig() { return plugin.getConfig(); }
 }
