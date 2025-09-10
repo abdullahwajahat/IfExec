308e85d6c1ef8d713c763b0b0b8a6a2e68c60188
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-10 23:04:17 +0500

    Update Messages.java

--- src/main/java/com/ifexec/manager/Messages.java
+++ src/main/java/com/ifexec/manager/Messages.java
@@ -2,7 +2,6 @@ package com.ifexec.manager;
 
 import com.ifexec.IfExec;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
@@ -10,7 +9,7 @@ import java.io.File;
 public class Messages {
     private final IfExec plugin;
     private File messagesFile;
-    private FileConfiguration messagesConfig;
+    private YamlConfiguration messagesConfig;
 
     public Messages(IfExec plugin) {
         this.plugin = plugin;
@@ -23,14 +22,12 @@ public class Messages {
         messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
     }
 
-    // raw message (no automatic prefix)
     public String get(String path) {
         String msg = messagesConfig.getString(path, "");
         if (msg == null) msg = "";
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
-    // message with prefix (prefix from file)
     public String getWithPrefix(String path) {
         String prefix = messagesConfig.getString("plugin_prefix", "");
         String msg = messagesConfig.getString(path, "");
