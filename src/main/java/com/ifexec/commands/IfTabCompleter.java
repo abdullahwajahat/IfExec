a7797e1180b8f0f3c1f18e8c82777f8680cf12f4
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-10 21:30:24 +0500

    Update IfTabCompleter.java

--- src/main/java/com/ifexec/commands/IfTabCompleter.java
+++ src/main/java/com/ifexec/commands/IfTabCompleter.java
@@ -18,66 +18,26 @@ public class IfTabCompleter implements TabCompleter {
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
         List<String> res = new ArrayList<>();
 
-        try {
-            // first argument suggestions
-            if (args.length == 1) {
-                List<String> subs = Arrays.asList("help","list","listall","remove","disable","enable","reload","edit","undo","on","isin");
-                for (String s : subs) if (s.startsWith(args[0].toLowerCase())) res.add(s);
-                // add trigger names
-                for (String n : plugin.getTriggerManager().names()) if (n.startsWith(args[0])) res.add(n);
-                return res.stream().distinct().collect(Collectors.toList());
-            }
-
-            // second argument suggestions
-            if (args.length == 2) {
-                String first = args[0].toLowerCase();
-                if (first.equals("remove") || first.equals("disable") || first.equals("enable") || first.equals("list") || first.equals("edit")) {
-                    for (String n : plugin.getTriggerManager().names()) if (n.startsWith(args[1])) res.add(n);
-                    return res;
-                }
-            }
-
-            // edit field suggestions
-            if (args.length >= 3 && args[0].equalsIgnoreCase("edit")) {
-                switch (args[2].toLowerCase()) {
-                    case "role": return Arrays.asList("staff","all");
-                    case "silent": return Arrays.asList("true","false");
-                    case "cooldown": return Arrays.asList("1","3","5","10","30");
-                }
-            }
-
-            // coords suggestion for "on" or "isin"
-            if ((args[0].equalsIgnoreCase("on") || args[0].equalsIgnoreCase("isin")) && sender instanceof Player) {
-                Player p = (Player) sender;
-                int x = p.getLocation().getBlockX();
-                int y = p.getLocation().getBlockY();
-                int z = p.getLocation().getBlockZ();
-                if (args[0].equalsIgnoreCase("on")) {
-                    return Arrays.asList(String.valueOf(x), String.valueOf(y), String.valueOf(z), p.getWorld().getName());
-                } else {
-                    return Arrays.asList(String.valueOf(x), String.valueOf(y), String.valueOf(z));
-                }
-            }
-
-            // server command suggestions after "then"
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].equalsIgnoreCase("then") || args[0].equalsIgnoreCase("commands")) {
-                    Set<String> cmdSet = new HashSet<>();
-                    for (org.bukkit.plugin.Plugin pl : Bukkit.getPluginManager().getPlugins()) {
-                        if (pl.getDescription().getCommands() != null)
-                            cmdSet.addAll(pl.getDescription().getCommands().keySet());
-                    }
-                    return cmdSet.stream().filter(s -> !s.toLowerCase().contains("worldedit")).sorted().collect(Collectors.toList());
-                }
-            }
-
-        } catch (Exception ex) {
-            plugin.getLogger().warning("TabComplete error: " + ex.getMessage());
+        if (args.length == 1) {
+            res.addAll(Arrays.asList("help","reload","list","listall","remove","enable","disable","edit","undo","on","isin"));
+            res.addAll(Bukkit.getOnlinePlayers().stream().map(Player::getName).toList());
+            res.addAll(Arrays.asList("@p","@a","@s"));
+        } else if (args[0].equalsIgnoreCase("edit") && args.length == 3) {
+            res.addAll(Arrays.asList("coords","role","commands","cooldown","silent","message"));
+        } else if (args[0].equalsIgnoreCase("edit") && args.length == 4 && args[2].equalsIgnoreCase("role")) {
+            res.addAll(Arrays.asList("staff","all"));
+        } else if (args[0].equalsIgnoreCase("edit") && args.length == 4 && args[2].equalsIgnoreCase("silent")) {
+            res.addAll(Arrays.asList("true","false"));
+        } else if (args[0].equalsIgnoreCase("on") && sender instanceof Player p) {
+            res.addAll(Arrays.asList(String.valueOf(p.getLocation().getBlockX()),
+                                     String.valueOf(p.getLocation().getBlockY()),
+                                     String.valueOf(p.getLocation().getBlockZ()),
+                                     p.getWorld().getName()));
         }
 
-        return res;
+        return res.stream().filter(s -> s.toLowerCase().startsWith(args[args.length-1].toLowerCase())).distinct().collect(Collectors.toList());
     }
 }
